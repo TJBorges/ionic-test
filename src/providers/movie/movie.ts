b2e9999b15b11d4371b8fe6ad32127e43416643f
TJ Borges <tj.batera1@gmail.com>
2018-04-16 23:51:28 -0300

    teste revert teste de commit excluido""
    This reverts commit cd4e653d59829bf0c49c420ec9d0ea5df375b6d6.

--- src/providers/movie/movie.ts
+++ src/providers/movie/movie.ts
@@ -10,6 +10,10 @@ export class MovieProvider {
     console.log('Hello MovieProvider Provider');
   }
 
+<<<<<<< HEAD
+=======
+  //Teste de commit excluido
+>>>>>>> parent of cd4e653... Revert "teste de commit excluido"
 
   getLatestMovies(){
     return this.http.get(this.baseApiPath + "/movie/now_playing?api_key=7e40a7b47945894173a6957518ad79e1");
