e6183d74e7aaac14e2136e233e04f4b27523f607
TJ Borges <tj.batera1@gmail.com>
2018-04-16 23:10:12 -0300

    teste de commit excluido

--- src/providers/movie/movie.ts
+++ src/providers/movie/movie.ts
@@ -1,12 +1,6 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the MovieProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class MovieProvider {
 
@@ -15,6 +9,7 @@ export class MovieProvider {
   constructor(public http: Http) {
     console.log('Hello MovieProvider Provider');
   }
+
 
   getLatestMovies(){
     return this.http.get(this.baseApiPath + "/movie/now_playing?api_key=7e40a7b47945894173a6957518ad79e1");
